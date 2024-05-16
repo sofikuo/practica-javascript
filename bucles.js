c58f09287a4ed5050e9957345553e704d5a19b7a
sofikuo <skuo@udesa.edu.ar>
2024-05-15 22:40:59 -0300

    agregue el b y el c del ej 1 de bucles

--- bucles.js
+++ bucles.js
@@ -5,3 +5,12 @@ for (let i = 0; i < 5; i++) {
 }
 
 //b//
+for (let i = 0; i < 10; i++) {
+    console.log(`La variable i tiene el valor ${i} `);
+}
+
+//c//
+for (let i = 0; i < 10; i+2) {
+    console.log(2 * i);
+}
+// para que varie de 2 en 2, en el primer renglon, pondria for (let i = 0; i < 10; i+2)ARREGLAR EL PRIMER FOR
