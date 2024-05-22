c3289d6afd025787367ea2e76e013eea9965677a
sofikuo <skuo@udesa.edu.ar>
2024-05-22 18:11:42 -0300

    hice hasta el 1d

--- practica_dom/practica_1/ejercicio1.js
+++ practica_dom/practica_1/ejercicio1.js
@@ -2,12 +2,14 @@ let bienvenida = "Bienvenidos a mi sitio.";
 let pregun_avanzar = confirm("¿Está seguro de querer avanzar?");
 
 if (pregun_avanzar == true) {
-    let salu = document.querySelector("h2").innerText("Qué alegría que quieras continuar tu visita");
+    let salu = document.querySelector("h2.saludo").innerText += "Qué alegría que quieras continuar tu visita";
 }
 else {
-    let salu = document.querySelector(".h2") + "Lamentamos que no quieras continuar tu visita"
+    let salu = document.querySelector("h2.saludo").innerText = "Lamentamos que no quieras continuar tu visita";
 }
 
-let ingresar_nombre = "Ingrese su nombre: "
-let nombre = ingresar_nombre
+let ingresar_nombre = "Ingrese su nombre: ";
+let nombre = ingresar_nombre;
 
+let cambio_h1 = document.querySelector("h1");
+let saludin = cambio_h1.innerText(nombre);
