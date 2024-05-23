95f369327d5e7e47b552fa89e8b952409940edee
sofikuo <skuo@udesa.edu.ar>
2024-05-23 18:21:40 -0300

    cambios

--- practica_dom/DOMPracticaBienvenidos/ejercicio1.js
+++ practica_dom/DOMPracticaBienvenidos/ejercicio1.js
@@ -1,15 +1,18 @@
 let bienvenida = "Bienvenidos a mi sitio.";
 alert(bienvenida);
-let pregun_avanzar = confirm("¿Está seguro de querer avanzar?");
 
-if (pregun_avanzar == true) {
-    let salu = document.querySelector("h2.saludo").innerText += "Qué alegría que quieras continuar tu visita";
-    return salu;
+let pregun_avanzar = confirm("¿Está seguro de querer avanzar?");
+let salu = document.querySelector("h2.saludo");
+
+
+if (salu == true) {
+    let respuesta = salu.innerText("Qué alegría que quieras continuar tu visita")
+    return
     
 }
 else {
-    let salu = document.querySelector("h2.saludo").innerText = "Lamentamos que no quieras continuar tu visita";
-    return salu;
+    let salu = document.querySelector("h2.saludo").innerText = "Lamentamos que no quieras continuar tu visita"
+    return
     
 }
 
