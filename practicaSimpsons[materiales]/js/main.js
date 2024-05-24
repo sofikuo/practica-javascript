f5d3f70fc5c03b841f7724434fa99495dd8f2f1d
sofikuo <skuo@udesa.edu.ar>
2024-05-24 10:36:10 -0300

    pegue la practica de los simpsons

---
+++ practicaSimpsons[materiales]/js/main.js
@@ -0,0 +1,20 @@
+// completar las variables para que el programa funcione
+
+window.addEventListener('load', function() {
+
+    //capturar HTML 
+    let titulo = ;
+ 
+    let saludo = ;
+ 
+    let span = ;
+ 
+    /* Capturar pesonajes + Asignarles a cada uno su evento correspondiente, 
+    así como también su funcionalidad */
+    
+
+    /* Asignarle evento a la variable boton */
+    let boton = ;
+ 
+ 
+ }) 
