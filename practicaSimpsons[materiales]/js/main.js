b8d948353099f3bf0d2c3cd3140bd3d4f051e634
sofikuo <skuo@udesa.edu.ar>
2024-05-24 12:13:45 -0300

    hice los ej de simp, pero no termine

--- practicaSimpsons[materiales]/js/main.js
+++ practicaSimpsons[materiales]/js/main.js
@@ -3,18 +3,30 @@
 window.addEventListener('load', function() {
 
     //capturar HTML 
-    let titulo = ;
+    let titulo = document.querySelector("#titulo");
+    titulo.addEventListener("mouseover", function() {
+        let nombre = prompt("¿CÓMO TE LLAMÁS?")
+        
+    })
  
-    let saludo = ;
- 
-    let span = ;
+    saludo.addEventListener("submit", function() {
+        if (nombre == false || nombre === " ") {
+            let saludo = document.querySelector("#saludo").innerText = "BIENVENID@ USUARIO";
+
+        }
+        else {
+            document.querySelector.innerText = "BIENVENIDO " + nombre;
+        }
+    })
+
+   // let span = ;
  
     /* Capturar pesonajes + Asignarles a cada uno su evento correspondiente, 
     así como también su funcionalidad */
     
 
     /* Asignarle evento a la variable boton */
-    let boton = ;
+    //let boton = ;
  
  
  }) 
